57be287f70ac54edbcd72e068e21eba26e4e64dc
bariulmunshi <bariulsoft@gmail.com>
2023-09-17 22:41:12 +0600

    updated

--- test.js
+++ test.js
@@ -1,13 +1,8 @@
-let x = 5;
-
-let y = 10;
-
-if (x < y) {
-
- console.log("x is less than y");
-
-} else {
-
- console.log("x is greater than or equal to y");
-
+var roastGiven=0;
+while(roastGiven<7){
+  console.log( 'Roast den,Please!!')
+  console.log(roastGiven);
+//   roastGiven=roastGiven+1;
+//   roastGiven+=1;
+//   roastGiven++;
 }
